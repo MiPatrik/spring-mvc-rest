4cae94acf3e104c041642ad3137aeb7ae67f4f89
Mičulka Patrik <patrik.miculka@sykora.cz>
2020-06-08 20:01:37 +0200

    Customer new

--- src/main/java/guru/springfamework/controllers/v1/CustomerController.java
+++ src/main/java/guru/springfamework/controllers/v1/CustomerController.java
@@ -8,10 +8,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/api/v1/customers/")
+@RequestMapping("/api/v1/customers")
 public class CustomerController {
 
 	private final CustomerService customerService;
@@ -25,8 +27,13 @@ public class CustomerController {
 		return new ResponseEntity<>(new CustomerListDTO(customerService.getAllCustomers()), HttpStatus.OK);
 	}
 
-	@GetMapping("{id}")
+	@GetMapping("/{id}")
 	public ResponseEntity<CustomerDTO> getCategoryByName(@PathVariable Long id) {
 		return new ResponseEntity<>(customerService.getCustomerById(id), HttpStatus.OK);
+	}
+
+	@PostMapping
+	public ResponseEntity<CustomerDTO> createNewCustomer(@RequestBody CustomerDTO customerDTO) {
+		return new ResponseEntity<>(customerService.createNewCustomer(customerDTO), HttpStatus.CREATED);
 	}
 }
--- src/test/java/guru/springfamework/services/CustomerServiceTest.java
+++ src/test/java/guru/springfamework/services/CustomerServiceTest.java
@@ -13,6 +13,7 @@ import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.when;
 
@@ -69,5 +70,23 @@ public class CustomerServiceTest {
 		CustomerDTO customerDTO = customerService.getCustomerById(1L);
 
 		assertEquals("Michale", customerDTO.getFirstName());
+	}
+
+	@Test
+	public void createNewCustomer() {
+		CustomerDTO customerDTO = new CustomerDTO();
+		customerDTO.setFirstName("Jim");
+
+		Customer savedCustomer = new Customer();
+		savedCustomer.setFirstName(customerDTO.getFirstName());
+		savedCustomer.setLastName(customerDTO.getLastName());
+		savedCustomer.setId(1L);
+
+		when(customerRepository.save(any(Customer.class))).thenReturn(savedCustomer);
+
+		CustomerDTO savedDto = customerService.createNewCustomer(customerDTO);
+
+		assertEquals(customerDTO.getFirstName(), savedDto.getFirstName());
+		assertEquals("/api/v1/customer/1", savedDto.getCustomerUrl());
 	}
 }
