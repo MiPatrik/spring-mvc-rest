5f5d1da9a8a52e5264fc358a5b9f981af5e61c23
Mičulka Patrik <patrik.miculka@sykora.cz>
2020-06-08 20:57:40 +0200

    Customer patch

--- src/main/java/guru/springfamework/services/CustomerServiceImpl.java
+++ src/main/java/guru/springfamework/services/CustomerServiceImpl.java
@@ -47,6 +47,27 @@ public class CustomerServiceImpl implements CustomerService {
 		return saveAndReturnDTO(customer);
 	}
 
+	@Override
+	public CustomerDTO patchCustomer(Long id, CustomerDTO customerDTO) {
+		return customerRepository.findById(id).map(customer -> {
+
+			if (customerDTO.getFirstName() != null) {
+				customer.setFirstName(customerDTO.getFirstName());
+			}
+
+			if (customerDTO.getLastName() != null) {
+				customer.setLastName(customerDTO.getLastName());
+			}
+
+			CustomerDTO returnDto = customerMapper.customerToCustomerDTO(customerRepository.save(customer));
+
+			returnDto.setCustomerUrl("/api/v1/customer/" + id);
+
+			return returnDto;
+
+		}).orElseThrow(RuntimeException::new);
+	}
+
 	private CustomerDTO saveAndReturnDTO(Customer customer) {
 		Customer savedCustomer = customerRepository.save(customer);
 
